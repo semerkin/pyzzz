8945689640c89366aae1ea2de5380513cc057ffe
semerkin <semerkin771@gmail.com>
2016-09-26 16:05:30 +0600

    Create simple_event_notification.py

---
+++ simple_event_notification.py
@@ -0,0 +1,29 @@
+# pip install asterisk-ami
+
+
+import os
+import time
+
+from asterisk.ami import AMIClient
+
+
+def event_notification(source, event):
+    os.system('notify-send "%s" "%s"' % (event.name, str(event)))
+
+
+host = '127.0.0.1'
+user = 'hey'
+password = 'user'
+
+client = AMIClient(host)
+future = client.login(user, password)
+if future.response.is_error():
+    raise Exception(str(future.response))
+
+client.add_event_listener(event_notification)
+
+try:
+    while True:
+        time.sleep(10)
+except (KeyboardInterrupt, SystemExit):
+    client.logoff()
